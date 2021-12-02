15bf42df8d397d5c2b22f6114ba52b70485afe50
jodrasko <youremail@provider.com>
2021-12-01 21:48:11 -0800

    Completed Mobile Design for Header Section

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,11 +1,17 @@
-import BrainFlixLogo from '../../assets/images/logo/BrainFlix-logo.svg';
-import './NavigationBar.scss';
+import BrainFlixLogo from "../../assets/images/logo/BrainFlix-logo.svg";
+import "./NavigationBar.scss";
 import UploadForm from "../UploadForm/UploadForm";
 
 function NavigationBar() {
   return (
     <header className="main-header">
-      <img src={BrainFlixLogo} alt="brainflix logo" className="main-header__logo" />
+      <div>
+        <img
+          src={BrainFlixLogo}
+          alt="brainflix logo"
+          className="main-header__logo"
+        />
+      </div>
       <UploadForm />
     </header>
   );
--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -5,13 +5,13 @@ function UploadForm() {
     <div className="uploadform__container">
       <form className="uploadform__form">
         <input
+          className="uploadform__input"
           type="text"
           name="userName"
           id="name"
           minlength="2"
           maxlength="30"
           placeholder="Search"
-          className="uploadform__input"
         />
 
         <div className="uploadform__box">
