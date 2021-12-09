85cf3301e2baa719066c7db05ec827da9e615a93
jodrasko <youremail@provider.com>
2021-12-08 22:29:18 -0800

    Implement VideoUploadForm for mobile and tablet

--- src/components/VideoUploadForm/VideoUploadForm.jsx
+++ src/components/VideoUploadForm/VideoUploadForm.jsx
@@ -1,7 +1,65 @@
 import "./VideoUploadForm.scss";
+import UploadVideoPreview from "../../assets/images/Upload-video-preview.jpg";
 
 function VideoUploadForm() {
-  return <div>VideoUploadForm Component</div>;
+  return (
+    <section className="publish-form">
+      <h2 className="publish-form__title">Upload Video </h2>
+      <form className="publish-form__form">
+        <div className="publish-form__input-container">
+          <label className="publish-form__label" htmlFor="video-thumbnail">
+            VIDEO THUMBNAIL
+          </label>
+          <img
+            src={UploadVideoPreview}
+            alt="upload video preview"
+            className="publish-form__thumbnail"
+            id="video-thumbnail"
+          />
+        </div>
+
+        <div className="publish-form__input-container">
+          <label className="publish-form__label" htmlFor="title">
+            TITLE YOUR VIDEO
+          </label>
+          <input
+            className="publish-form__input"
+            type="text"
+            name="title"
+            id="title"
+            minLength="2"
+            maxLength="30"
+            placeholder="Add a title to your video"
+            required
+          />
+        </div>
+
+        <div className="publish-form__input-container">
+          <label className="publish-form__label" htmlFor="description">
+            ADD A VIDEO DESCRIPTION
+          </label>
+          <textarea
+            className="publish-form__description"
+            name="description"
+            id="description"
+            placeholder="Add a description to your video"
+            required
+          ></textarea>
+        </div>
+
+        <div className="publish-form__button-container">
+          <button type="submit" className="publish-form__button">
+            PUBLISH
+          </button>
+          {/* </div> */}
+          {/* <div className="publish-form__button-container"> */}
+          <button type="submit" className="publish-form__button-colorless">
+            CANCEL
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default VideoUploadForm;
